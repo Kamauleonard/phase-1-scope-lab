3acd58e3d3301a6fafafda080c3c36fd89f68b9d
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-04 13:21:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Solution 1
+var customerName;
+
+customerName = 'bob';
+
+// Solution 2
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+    }
+}
+// Solution 3
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Solution 4
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Solution 5
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Alex';
+}
